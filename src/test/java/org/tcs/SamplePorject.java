782a60d0dd57e1b7c2dafd3394f2ddf32ad7923b
jaya1804 <jaya1804apr@gmail.com>
2020-03-07 11:08:33 +0530

    Code for Gopi and Jaya

--- src/test/java/org/tcs/SamplePorject.java
+++ src/test/java/org/tcs/SamplePorject.java
@@ -4,6 +4,8 @@ public class SamplePorject {
 
 	public static void main(String[] args) {
 		System.out.println("3");
+		System.out.println("Gopi Code");
+		System.out.println("Code for jaya");
 		
 	}
 }
